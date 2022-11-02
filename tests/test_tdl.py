337b5465a2a9aadf8069883d12f6f5d6d14828c7
dwest77a <daniel.westwood@stfc.ac.uk>
2022-11-02 14:11:24 +0000

    Added git workflows code and requirements to test tdl

---
+++ tests/test_tdl.py
@@ -0,0 +1,44 @@
+import pytest
+import sys
+sys.path.insert(0,'..')
+
+entry_empty = {
+    "Format": "", 
+    "Description": "", 
+    "Type": "", 
+    "Dependency": "", 
+    "Date": ""
+}
+
+entry_filled = {
+    "Format": "test", 
+    "Description": "test", 
+    "Type": "test", 
+    "Dependency": "test", 
+    "Date": "test"
+}
+
+json_base = {
+    "0": entry_empty,
+    "1": entry_filled,
+    "2": entry_empty
+}
+
+json_reorder = {
+    "0": entry_filled,
+    "1": entry_empty,
+    "2": entry_empty
+}
+
+
+@pytest.mark.parametrize(
+    "json_in, json_out, reorder_str", [
+        (json_base, json_reorder, "1 0 2")
+    ]
+)
+def test_reorder(json_in, json_out, reorder_str):
+    from src.tdlman import reorder
+
+    json_check = reorder(json_in, reorder_str=reorder_str)
+
+    assert json_check == json_out
